12f4d4d7ed863fa26907a43b189c8022cf7fb361
Claudia <claudia.g.escribano@gmail.com>
2016-01-27 10:13:26 +0100

    Second attempt for max min in a rasterstack

--- maxmin.R
+++ maxmin.R
@@ -10,7 +10,8 @@ data <- stack("data.nc")
 
 maximos <- function(x){
     MM <- maxValue(max(x)) ## Encuentra el valor númerico máximo de la capa máxima del rasterStack
-    xyMM <- xyFromCell(max(x), MM) ## Posición lon lat del máximo
+    imax <- which.max(max(x)) ## te dice en qué celda se encuentra el máximo.
+    xyMM <- xyFromCell(max(x), imax) ## Posición lon lat del máximo
     maxL <- which.max(x) ## calcula para cada celda la capa en la que se da el valor máximo
     Ly <- extract(maxL, xyMM) ## Para la posición de nuestro máximo extraemos el valor de la capa que nos dice cuándo s eda el máximo.
     sol <- c(MM, xyMM, Ly) ## Es el vector solución que contiene toda la información antes calculada.
@@ -21,7 +22,8 @@ maximos <- function(x){
 
 minimos <- function(x){
     mm <- minValue(min(x))
-    xymm <- xyFromCell(min(x), mm)
+    imin <-which.min(min(x))
+    xymm <- xyFromCell(min(x), imin)
     minL <- which.min(x)
     Ly <- extract(minL, xymm)
     sol <- c(mm, xymm, Ly)
@@ -33,7 +35,8 @@ minimos <- function(x){
 maxmin <- function(x, y) {
     if (y == 'max' ){
         MM <- maxValue(max(x))
-        xyMM <- xyFromCell(max(x), MM)
+        imax <- which.max(max(x))
+        xyMM <- xyFromCell(max(x), imax)
         maxL <- which.max(x)
         Ly <- extract(maxL, xyMM)
         sol <- c(MM, xyMM, Ly)
@@ -41,7 +44,8 @@ maxmin <- function(x, y) {
     }
     else {
         mm <- minValue(min(x))
-        xymm <- xyFromCell(min(x), mm)
+        imin <- which.min(min(x))
+        xymm <- xyFromCell(min(x), imin)
         minL <- which.min(x)
         Ly <- extract(minL, xymm)
         sol <- c(mm, xymm, Ly)
