4b698fc6b6d9f686ad1e56f31a3c18e3283222df
Claudia <claudia.g.escribano@gmail.com>
2016-05-25 12:16:00 +0200

    'Selecting optimal partition'

--- clustering.R
+++ clustering.R
@@ -59,6 +59,72 @@ Indice <- lapply(resultado, FUN='CH')
 Indice <- do.call(cbind, Indice) 
 
 
-## Once we have applied the index, we take the optimal partition and visualizes it.
+## Once we have applied the index, we take the optimal partition and visualize it.
+
+## How can I find the median vector on the distribution?
+
+## Calc of the rmse of the lines like in the paper of Zagouras. (CALCULO DE LOS n RMSEc. Para ello, necesito calcular para cada exp n rmse y escoger el menor.)
+
+rmse <- list()
+
+ajuste <- function(x){
+    
+            for(j in 1:67){
+        r1L <- lm(x[1:j+1]~c(1:j+1))
+        r1R <- lm(x[j+2:70]~c(j+2:70))
+    rmse[[j]] <- c(sqrt(sum((r1L$residuals)^2)), sqrt(sum((r1R$residuals)^2)))}
+    return(rmse)
+}
+
+## I apply this 'ajuste' function to every element of 'Indice' (they are lists of kmeans results)
+
+rmse_Exp <- apply(Indice, 2, FUN='ajuste')
+
+## Ponderate the rmse from lm results.
+ajustePond <- function(x){
+    rmseT <- c()
+        for(i in 1:67)
+    rmseT[i] <-((i+1)-1)/((70)-1)*(x[[i]][1])+(70-(i+1))/(70-1)*(x[[i]][2])
+    return(rmseT)
+}
 
 
+rmse_ExpP <- lapply(rmse_Exp, FUN='ajustePond')
+
+## list of 'n' elements with smaller RMSE:
+
+minRmse <- lapply(rmse_ExpP, FUN=function(x) unlist(x)) ## list of n elements with smaller RMSE
+
+minRmse_w<- lapply(minRmse, FUN=function(x) min(x))
+
+## Which of the n is on the middle
+
+middle <- function(x) {
+vectorMIN <- c(unlist(x))
+mediano <- sort(vectorMIN)[50]
+
+posicion <-which(vectorMIN == mediano) 
+return(posicion)
+}
+
+middle(minRmse_w) ## Te da la posición de la partición mediana.
+
+
+## Me voy a la posición 'middle(minRmse_w)' de la lista que contiene los RMSE para todos los ajustes. Tengo que ver qué porsición corresponde al valor mínimo:
+
+kopt <- min(rmse_ExpP[['middle(minRmse_w)']]) ## en los corchetes tengo que poner la posición que he obtenido con middle(minRmse_w)
+kpos <- which(rmse_ExpP[['middle(minRmse_w)']] == kopt) ## Lo mismo
+kpos
+
+## Una ve que tengo la partición óptima, tengo que representarla.
+
+## El numero de clusters siempre es kpos+1
+
+## partición ótima:
+
+optima <- resultado[['middle']][['kpos']]
+
+clusters <- raster(my_nc)
+clusters <- setValues(clusters, optima$cluster)
+
+levelplot(clusters)
